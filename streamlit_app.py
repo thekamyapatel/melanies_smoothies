811163cbaf69e255921401913999b3b8778ac71e
thekamyapatel <thekamyapatel@gmail.com>
2025-07-09 13:45:07 -0400

    Update streamlit_app.py
    please work

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,6 +31,10 @@ if ingredients_list:
 
     #st.write(ingredients_string)
 
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        # st.text(smoothiefroot_response.json())
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+      
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """', '"""+name_on_order+ """')"""
 
@@ -41,7 +45,3 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         
         st.success(f"✅ Your Smoothie is ordered, {name_on_order}!")
-
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-      # st.text(smoothiefroot_response.json())
-      sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
